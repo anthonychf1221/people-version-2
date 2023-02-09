1010ecd90933afc89183abf468f76f96cea55b8e
Anthony Chau Frias <anthonysy21@gmail.com>
2023-02-09 03:38:48 -0500

    test: add test to validate the people sis

---
+++ app/src/test/java/com/anthonychaufrias/people/LiveDataUtilTest.kt
@@ -0,0 +1,31 @@
+package com.anthonychaufrias.people
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+fun<T> LiveData<T>.getOrAwaitValue(): T{
+    var data: T? = null
+    var latch = CountDownLatch(1)
+
+    val observer = object : Observer<T>{
+        override fun onChanged(t: T) {
+            data = t
+            this@getOrAwaitValue.removeObserver(this)
+            latch.countDown()
+        }
+    }
+    this.observeForever(observer)
+
+    try {
+        if (!latch.await(20, TimeUnit.SECONDS)) {
+            throw TimeoutException("Live Data never gets its value")
+        }
+    }
+    finally {
+        this.removeObserver(observer)
+    }
+    return data as T
+}
---
+++ app/src/test/java/com/anthonychaufrias/people/mock/PersonMockService.kt
@@ -0,0 +1,50 @@
+package com.anthonychaufrias.people.mock
+
+import com.anthonychaufrias.people.data.model.Person
+import com.anthonychaufrias.people.data.model.PersonListResponse
+import com.anthonychaufrias.people.data.model.PersonSaveResponse
+import com.anthonychaufrias.people.data.service.IPersonService
+import retrofit2.Response
+
+class PersonMockService: IPersonService {
+
+    override suspend fun getPeopleList(search: String): Response<PersonListResponse> {
+        val results = mutableListOf<Person>()
+        results.add(Person(1, "Anthony Chau", "12345678", 1, "Perú"))
+        results.add(Person(2, "Juana Miranda", "22222222", 2, "Colombia"))
+        results.add(Person(3, "Francisco Tirado", "33333333", 2, "Colombia"))
+        results.add(Person(4, "Juanita Gutierrez", "44444444", 1, "Perú"))
+        results.add(Person(5, "Guillermina Lubarti", "55555555", 3, "Argentina"))
+        results.add(Person(6, "Juan Perez", "55558888", 3, "Argentina"))
+
+        var listFiltered = mutableListOf<Person>()
+        val filter = search.lowercase()
+
+        listFiltered = if (search.isEmpty())
+            results
+        else {
+            for (item in results.indices) {
+                if( results[item].fullName.lowercase().contains(filter) ){
+                    listFiltered.add(results[item])
+                }
+            }
+            listFiltered
+        }
+
+        val listResponse = PersonListResponse("Ok", listFiltered)
+        return Response.success(listResponse)
+    }
+
+    override suspend fun addPerson(person: Person): Response<PersonSaveResponse> {
+        val saveResponse = PersonSaveResponse("Ok", person, "")
+        return Response.success(saveResponse)
+    }
+
+    override suspend fun updatePerson(person: Person): Response<PersonSaveResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deletePerson(person: Person): Response<PersonSaveResponse> {
+        TODO("Not yet implemented")
+    }
+}
