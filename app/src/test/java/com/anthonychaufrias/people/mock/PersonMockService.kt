3028d73c6de61c8590b4dafe74736188d486a590
Anthony Chau Frias <anthonysy21@gmail.com>
2023-02-10 16:25:31 -0500

    test(people): add test to verify people list
    add test and mock data(to mock the API responses)

---
+++ app/src/test/java/com/anthonychaufrias/people/mock/PersonMockService.kt
@@ -0,0 +1,90 @@
+package com.anthonychaufrias.people.mock
+
+import com.anthonychaufrias.people.data.model.Person
+import com.anthonychaufrias.people.data.model.PersonListResponse
+import com.anthonychaufrias.people.data.model.PersonSaveResponse
+import com.anthonychaufrias.people.data.service.IPersonService
+import retrofit2.Response
+
+class PersonMockService: IPersonService {
+    private val results = mutableListOf<Person>()
+    init {
+        loadFakeData()
+    }
+
+    companion object{
+        val ANTHONY_CHAU_FRIAS = Person(1, "Anthony Chau", "12345678", 1, "Perú")
+        val GUILLERMINA_LUBARTI = Person(5, "Guillermina Lubarti", "55555555", 3, "Argentina")
+    }
+
+    override suspend fun getPeopleList(search: String): Response<PersonListResponse> {
+        var listFiltered = mutableListOf<Person>()
+        val filter = search.lowercase()
+
+        listFiltered = if (search.isEmpty())
+            results
+        else {
+            for (item in results.indices) {
+                if( results[item].fullName.lowercase().contains(filter) ){
+                    listFiltered.add(results[item])
+                }
+            }
+            listFiltered
+        }
+
+        val listResponse = PersonListResponse("Ok", listFiltered)
+        return Response.success(listResponse)
+    }
+
+    override suspend fun addPerson(person: Person): Response<PersonSaveResponse> {
+        var status = ""
+        var message = ""
+        if(findPersonBy(person.documentID, person.personID) == null ){
+            person.personID = results.size
+            status = "Ok"
+        }
+        else{
+            status = "Failed"
+            message = "Documento duplicado. Por favor ingrese otro"
+        }
+
+        val saveResponse = PersonSaveResponse(status, person, message)
+        return Response.success(saveResponse)
+    }
+
+    override suspend fun updatePerson(person: Person): Response<PersonSaveResponse> {
+        var status = ""
+        var message = ""
+        if(findPersonBy(person.documentID, person.personID) == null ){
+            status = "Ok"
+        }
+        else{
+            status = "Failed"
+            message = "Documento duplicado. Por favor ingrese otro"
+        }
+
+        val saveResponse = PersonSaveResponse(status, person, message)
+        return Response.success(saveResponse)
+    }
+
+    override suspend fun deletePerson(person: Person): Response<PersonSaveResponse> {
+        TODO("Not yet implemented")
+    }
+
+    private fun findPersonBy(documentID: String, personID: Int): Person?{
+        return results.find { person ->
+            person.documentID == documentID &&
+                    person.personID != personID
+        }
+    }
+
+    private fun loadFakeData(){
+        results.add(ANTHONY_CHAU_FRIAS)
+        results.add(Person(2, "Juana Miranda", "22222222", 2, "Colombia"))
+        results.add(Person(3, "Francisco Tirado", "33333333", 2, "Colombia"))
+        results.add(Person(4, "Juanita Gutierrez", "44444444", 1, "Perú"))
+        results.add(GUILLERMINA_LUBARTI)
+        results.add(Person(6, "Juan Perez", "55558888", 3, "Argentina"))
+    }
+
+}
---
+++ app/src/test/java/com/anthonychaufrias/people/ui/viewmodel/PersonViewModelTest.kt
@@ -0,0 +1,61 @@
+package com.anthonychaufrias.people.ui.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.anthonychaufrias.people.data.PersonRepository
+import com.anthonychaufrias.people.data.service.PersonService
+import com.anthonychaufrias.people.domain.SetPersonUseCase
+import com.anthonychaufrias.people.domain.UpdatePersonUseCase
+import com.anthonychaufrias.people.getOrAwaitValue
+import com.anthonychaufrias.people.mock.PersonMockService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PersonViewModelTest{
+    @get: Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var personViewModel: PersonViewModel
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(mainThreadSurrogate)
+
+        val service = PersonMockService()
+        val api = PersonService(service)
+        val repository = PersonRepository(api)
+
+        val setPersonUseCase = SetPersonUseCase(repository)
+        val updatePersonUseCase = UpdatePersonUseCase(repository)
+
+        personViewModel = PersonViewModel(repository, setPersonUseCase, updatePersonUseCase)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun `validate the size of the entire people list`() {
+        // given
+        val search: String = ""
+        personViewModel.peopleList.clear()
+
+        // when
+        personViewModel.loadPeopleList(search)
+
+        // then
+        val result = personViewModel.liveDataPeopleList.getOrAwaitValue()
+        assertTrue(result.size == 6)
+        assertTrue(personViewModel.peopleList.size == 6)
+    }
+}
